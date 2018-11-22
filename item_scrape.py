d37f85defc68dd5d4f4ffe19fdc9dffd399a29df
VMuliadi <vinsen@accelbyte.net>
2018-11-22 23:38:22 +0700

    Adding JSON details

--- item_scrape.py
+++ item_scrape.py
@@ -4,9 +4,10 @@
 # Scraping data from Ragnarok Online Mobile : Eternal Love Wiki
 
 from bs4 import BeautifulSoup
+import json
 import requests
 import os
-import pymongo
+# import pymongo
 
 MONGO_HOSTNAME = ""
 MONGO_USERNAME = ""
@@ -23,19 +24,54 @@ MONGO_PASSWORD = ""
 # 	item_description = BeautifulSoup(scrape_text, 'html.parser').findAll('div', {'class': ''})
 # 	item_drop_percentage = BeautifulSoup(scrape_text, 'html.parser').findAll('div', {'class': ''})
 
-list_of_scrapable_items = []
 base_url_for_item_list = 'https://www.roguard.net/db/items/'
-get_latest_page = int(BeautifulSoup(requests.get(base_url_for_item_list).text, 'html.parser').findAll('a', {'class': 'page-link'})[-1].get_text())
+get_latest_page = int(BeautifulSoup(requests.get(base_url_for_item_list).text, 'html.parser')\
+	.findAll('a', {'class': 'page-link'})[-1]\
+	.get_text())
 
 
-for index in range(1, get_latest_page): 
+def get_item_list():
+	for index in range(1, get_latest_page): 
+		if index == 1: base_url = base_url_for_item_list
+		else:  base_url = base_url_for_item_list + '?page=' + str(index)
+		list_of_scraped_item = set()
+		for table_row in BeautifulSoup(requests.get(base_url).text, 'html.parser')\
+			.findAll('div', {'style': 'font-size: 1.2em;'}):
+				list_of_scraped_item.add(
+					table_row
+					.findAll('a')[0]\
+					.get_text()\
+					.encode(encoding='utf-8') + '\n'
+				)
+
+		with open('item_list.txt', 'a') as write_item:
+			for item in list_of_scraped_item:
+				write_item.write(item)
+
+
+for index in range(1, get_latest_page):
+	list_of_scraped_item = []
 	if index == 1: base_url = base_url_for_item_list
 	else:  base_url = base_url_for_item_list + '?page=' + str(index)
-	list_of_scraped_item = set()
 	for table_row in BeautifulSoup(requests.get(base_url).text, 'html.parser').findAll('div', {'style': 'font-size: 1.2em;'}):
-		list_of_scraped_item.add(table_row.findAll('a')[0].get_text().encode(encoding='utf-8') + '\n')
+		try : 
+			item_details = {}
+			item_details['item_name'] = table_row.get_text().encode(encoding='ascii')
+			print('Processing ' + item_details['item_name'])
+			item_url = 'https://www.roguard.net' + table_row.findAll('a')[0].get('href')
+			item_url_scrape = BeautifulSoup(requests.get(item_url).text, 'html.parser')
+			try: item_details['item_description'] = item_url_scrape.findAll('p', {'style': 'padding: 10px;'})[0].get_text()
+			except: item_details['item_description'] = 'Not Available'
+			item_information = item_url_scrape.findAll('td', {'class': 'text-right'})
+			item_details['level'] = int(item_information[0].get_text())
+			item_details['max_stack'] = int(item_information[1].get_text())
+			item_details['is_sellable'] = True if item_information[2].get_text() == 'Yes' else False
+			item_details['sell_price'] = str(item_information[3].get_text())
+			item_details['is_auctionable'] = True if item_information[4].get_text() == 'Yes' else False
+			item_details['is_storagable'] = True if item_information[5].get_text() == 'Yes' else False
+			list_of_scraped_item.append(json.dumps(item_details))
+		except UnicodeEncodeError: pass  # Not international item
 
-	with open('item_list.txt', 'a') as write_item:
-		for item in list_of_scraped_item:
-			write_item.write(item)
-
+	for item in list_of_scraped_item:
+		with open('item_details.txt', 'a') as item_details:
+			item_details.write(item + '\n')
